394e818becf9a1611f1f4e81acd7a404ece7d2e4
Hubert Thieriot <hubert@energyandcleanair.org>
2021-03-31 15:52:39 +0800

    -missing file

---
+++ data.R
@@ -0,0 +1,46 @@
+
+data.get_meas <- function(use_cache=T, polls, date_from, date_to, years_rel){
+
+  f <- file.path("results","data","meas.RDS")
+
+  if(file.exists(f) && use_cache){
+    readRDS(f)
+  }else{
+    meas <- do.call("bind_rows",
+                    lapply(years_rel, function(year_rel){
+                      print(year_rel)
+                      rcrea::measurements(
+                        date_from=lubridate::date(date_from)+lubridate::years(year_rel),
+                        date_to=lubridate::date(date_to)+lubridate::years(year_rel),
+                        source="mee",
+                        deweathered=F,
+                        poll=polls,
+                        with_metadata=T,
+                        with_geometry=F
+                      ) %>%
+                        mutate(season=paste(
+                          lubridate::year(date_from)+year_rel,
+                          lubridate::year(date_from)+year_rel+1,
+                          sep="-")) %>%
+                        ungroup()
+                    }))
+    saveRDS(meas, f)
+    return(meas)
+  }
+}
+
+
+data.gadm <- function(level=1){
+  # g <- sf::read_sf(file.path(Sys.getenv("DIR_DATA"),
+                             # sprintf("boundaries/gadm/gadm36_%d.shp",level)))
+  # g <- g[g$GID_0=="CHN",]
+  # g.simplified <- rmapshaper::ms_simplify(input = as(g, 'Spatial')) %>%
+    # st_as_sf()
+  return(sf::read_sf("data/boundaries/gadm_simplified.shp"))
+}
+
+data.keyregions <- function(){
+  read.csv("data/station_key2018.csv") %>%
+    distinct(location_name=CityEN, keyregion, province=Province)
+}
+
